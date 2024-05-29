7e7b369202aa48220583fbe3e62693f393caebc9
chiayinin <chiayinin@gmail.com>
2024-05-29 17:31:53 +0800

    feat: add GET API.

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 import * as http from 'http';
 import mongoose from 'mongoose';
+import Post from './modules/posts.js';
+
+const headers = {
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+  'Content-Type': 'application/json'
+}
 
 mongoose
   .connect('mongodb://localhost:27017/posts')
   .then(() => console.log("mongoose 資料庫連結成功"));
 
-const requestListener = (req, res) => {
-  console.log('路由:', req.url);
-  res.end();
+const requestListener = async (req, res) => {
+  let body = '';
+
+  req.on('data', chunk => {
+    body += chunk;
+  });
+
+  // 取得所有資料
+  if(req.url == '/posts' && req.method == 'GET') {
+    console.log('GET ALL');
+    const post = await Post.find(); // 透過 mongoose find() 方法取得所有資料
+
+    res.writeHead(200, headers);
+    res.write(JSON.stringify({
+      "status" : "success",
+      post
+    }));
+    res.end();
+  } else {
+    console.log('else');
+  }
 };
 
 const app = http.createServer(requestListener);
